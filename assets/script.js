03541b473c68cbcaf0bfae7238c0c18a996d5bf1
Nehoa21 <nehoaakina21@yahoo.com>
2023-09-17 16:17:23 -0600

    added questions and answers

--- assets/script.js
+++ assets/script.js
@@ -12,23 +12,54 @@
 // WHEN the game is over
 // THEN I can save my initials and my score
 
-// questions
+// questions and answers
 var questions = [
   {
-    question: ""
+    question: "Commonly used datatypes DO NOT include:",
+    answers: [
+      {text: "1. Strings", correct: false},
+      {text: "2. Booleans", correct: false},
+      {text: "3. Alerts", correct: false},
+      {text: "4. Numbers", correct: false}
+    ]
+  },
+  {
+    question: "The condition in an if / else statement is enclosed with:",
+    answers: [
+      {text: "1. Quotes", correct: false},
+      {text: "2. Curly brackets", correct: false},
+      {text: "3. Parentheses", correct: false},
+      {text: "4. Square brackets", correct: false}
+    ]
+  },
+  {
+    question: "Arrays in Javascript can be used to store ____.:",
+    answers: [
+      {text: "1. numbers and strings", correct: false},
+      {text: "2. other arrays", correct: false},
+      {text: "3. booleans", correct: false},
+      {text: "4. all of the above", correct: false}
+    ]
+  },
+  {
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    answers: [
+      {text: "1. commas", correct: false},
+      {text: "2. curly brackets", correct: false},
+      {text: "3. quotes", correct: false},
+      {text: "4. parentheses", correct: false}
+    ]
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      {text: "1. Javascript", correct: false},
+      {text: "2. Terminal/bash", correct: false},
+      {text: "3. for loops", correct: false},
+      {text: "4. Console log", correct: false}
+    ]
   }
-]
-var q2 = "";
-var q3 = "";
-var q4 = "";
-var q5 = "";
-
-// answers
-var answers1 = ["", "", "", ""];
-var amswers2 = ["", "", "", ""];
-var answers3 = ["", "", "", ""];
-var answers4 = ["", "", "", ""];
-var answers5 = ["", "", "", ""];
+];
 
 var correct = true;
 var timerCount = 75;
