a88871eb5a775158044cd731971429181c2a76cf
Nehoa21 <nehoaakina21@yahoo.com>
2023-09-16 21:01:02 -0600

    set up basic code

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+// start timer when start button pushed
+// function to get questions
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+// WHEN the game is over
+// THEN I can save my initials and my score
+
+
+
+
+
+
+
+
+
+// timer function
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 1000);
+  }
